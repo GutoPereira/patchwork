3dcf25e1db0174b66b83e5eead4b69e31a3c14e4
Guto Pereira <guto.pereira@live.com>
2018-10-06 00:56:57 -0300

    remove function from init

--- js/init.js
+++ js/init.js
@@ -1,12 +1,1 @@
 M.AutoInit();
-
-function change(elem) {
-  let e = elem.getElementsByTagName('i')[0];
-  if (e.innerHTML == "toggle_off") {
-    e.className = e.className.replace(/grey-text/,'orange-text');
-    e.innerHTML = "toggle_on";
-  } else {
-    e.className = e.className.replace(/orange-text/,'grey-text');
-    e.innerHTML = "toggle_off";
-  }
-}
